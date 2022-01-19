f7947f4f7465bd6d382b8809af9f2a4699112ad3
FabioMussolin <fabiomussolin@gmail.com>
2022-01-19 00:52:02 -0300

    Add files via upload

---
+++ bd_crud.sql
@@ -0,0 +1,74 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.5.5-10.4.22-MariaDB : Database - projetocrud
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`projetocrud` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `projetocrud`;
+
+/*Table structure for table `cliente` */
+
+DROP TABLE IF EXISTS `cliente`;
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(500) DEFAULT NULL,
+  `email` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `cliente` */
+
+insert  into `cliente`(`id`,`nome`,`email`) values (2,'Fabio Mattos','fabiomussolin@gmail.com'),(4,'Fabio Mattos','fabio.mattos@outlook.com.br');
+
+/*Table structure for table `fatura` */
+
+DROP TABLE IF EXISTS `fatura`;
+
+CREATE TABLE `fatura` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) DEFAULT NULL,
+  `valor` varchar(30) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `data_vencimento` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`),
+  CONSTRAINT `fatura_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `fatura` */
+
+insert  into `fatura`(`id`,`id_cliente`,`valor`,`data`,`data_vencimento`) values (31,2,'20','2022-01-18','2022-01-18'),(32,4,'12','2022-01-18','2022-01-20'),(33,4,'24','2022-01-18','2022-01-20');
+
+/*Table structure for table `produto` */
+
+DROP TABLE IF EXISTS `produto`;
+
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produto` varchar(500) DEFAULT NULL,
+  `valor` varchar(30) DEFAULT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`),
+  CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `produto` */
+
+insert  into `produto`(`id`,`produto`,`valor`,`id_cliente`) values (4,'Caneta','20.00',2),(5,'Caderno','12.00',4),(6,'Lapis','12.00',4);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
